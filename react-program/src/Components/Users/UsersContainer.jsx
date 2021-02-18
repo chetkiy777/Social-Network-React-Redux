769d16e44a17d4e0f4306adf4b04a7d077877079
chetkiy777 <71456087+chetkiy777@users.noreply.github.com>
2021-02-18 10:50:11 +0200

    add true project

---
+++ react-program/src/Components/Users/UsersContainer.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import {connect} from 'react-redux';
+import {
+    follow, getUsers, setCurrentPage,
+    toggleFollowingInProgress,
+    unfollow
+} from '../../redux/users-reducer';
+import Users from './Users';
+import Preloader from '../../common/preloader/Preloader'
+import {withAuthRedirect} from "../../hoc/withAuthRedirect";
+import {compose} from "redux";
+
+
+
+class UserContainer extends React.Component {
+    componentDidMount() {
+        this.props.getUsers(this.props.currentPage , this.props.pageSize)
+    }
+    onPageChange = (pageNumber) => {
+        this.props.getUsers(pageNumber, this.props.pageSize)
+    }
+    render() {
+        return <>
+            {this.props.isFetching ? <Preloader />: null}
+            <Users totalUsersCount={this.props.totalUsersCount}
+                      pageSize={this.props.pageSize}
+                      currentPage={this.props.currentPage}
+                      onPageChange={this.onPageChange}
+                      users={this.props.users}
+                      unfollow={this.props.unfollow}
+                      follow={this.props.follow}
+                      isFetching={this.props.isFetching}
+                      followingInProgress={this.props.followingInProgress}
+                      toggleFollowingInProgress={this.props.toggleFollowingInProgress} />
+            </>
+    }
+}
+
+let mapStateToProps = (state) => {
+    return {
+        users: state.usersPage.users,
+        pageSize: state.usersPage.pageSize,
+        totalUsersCount: state.usersPage.totalUsersCount,
+        currentPage: state.usersPage.currentPage,
+        isFetching: state.usersPage.isFetching,
+        followingInProgress: state.usersPage.followingInProgress
+
+    }
+}
+
+export default compose(
+    /*withAuthRedirect,*/
+    connect(mapStateToProps , {follow, unfollow, getUsers, setCurrentPage, toggleFollowingInProgress}),
+)(UserContainer)
+
+
+
